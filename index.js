71f89969f6bc64125068970adda3f33115e5c5e4
Emad Albalas <ealbalas@csu.fullerton.edu>
2023-04-02 21:50:09 -0700

    add background picture for each button in styles file. added click listener and keydown listener. linked sound file to each button as well

--- index.js
+++ index.js
@@ -5,5 +5,105 @@ for (let i = 0; i < btns.length; i++) {
 }
 
 function handleClick() {
-    alert("I got clicked!");
-}
+    var buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            console.log(buttonInnerHTML);
+            break;
+
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;  
+
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+        case "j":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+        case "k":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+        case "l":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+    }
+
+function handleKeyClick(event) {
+    var buttonInnerHTML = event.key;
+    switch (buttonInnerHTML) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            console.log(buttonInnerHTML);
+            break;
+
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;  
+
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+        case "j":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+        case "k":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+        case "l":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            console.log(buttonInnerHTML);
+            break;      
+
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+    }
+    
+document.addEventListener("keydown", handleKeyClick);
